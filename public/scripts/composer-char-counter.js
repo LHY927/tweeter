1f851e898700e2a3566a396504325e3d803c3aa9
LHY927 <liuhaiyue0927@gmail.com>
2024-01-29 23:03:04 -0500

    week 8

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $("#tweet-text").on("keyup",function(){
+        const max = 140;
+        let len = $(this).val().length;
+        $(".counter").text(max-len);
+        console.log($(this).closest("form"));
+        console.log($(this).closest("form").find(".counter"));
+        if(max <= len){
+            $(".counter").css("color","red");
+        }
+    })
+})
