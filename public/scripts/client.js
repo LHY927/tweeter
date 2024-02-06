544d1338530c0bb81241971d8d5aa206eb9da360
LHY927 <liuhaiyue0927@gmail.com>
2024-02-06 10:30:49 -0500

    changed the way of reload

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -45,26 +45,35 @@ const renderTweets = function (tweets) {
   // calls createTweetElement for each tweet
   // takes return value and appends it to the tweets container
   for (const tweet of tweets) {
-    let $tweet = createTweetElement(tweet);
-    $(".tweets-container").append($tweet);
+    renderNewTweet(tweet);
   }
 };
 
+const renderNewTweet = function(tweet){
+  let $tweet = createTweetElement(tweet);
+  $(".tweets-container").append($tweet);
+}
+
 const loadTweets = function (url) {
   $.ajax(url, { method: "GET" }).then(function (data) {
-    renderTweets(data);
-    //console.log(data);
+   renderTweets(data);
   });
 };
 
+const loadNewTweet = function(url){
+  $.ajax(url, { method: "GET" }).then(function (data) {
+    let latest = data.length - 1;
+    renderNewTweet(data[latest]);
+    console.log(data);
+  });
+}
+
 const showErrorMessage = function (msg) {
-  $(".error-message").append(`<i class="fa-solid fa-triangle-exclamation"></i>
+  $(".error-message").empty().append(`<i class="fa-solid fa-triangle-exclamation"></i>
     <span>${msg}</span>
     <i class="fa-solid fa-triangle-exclamation"></i>`);
   $(document).ready(function () {
-    $(".error-message").slideDown("slow", function () {
-      console.log("works");
-    });
+    $(".error-message").slideDown("slow");
   });
 };
 
@@ -72,24 +81,19 @@ $(window).on("load", function () {
   loadTweets(url);
   $("form").on("submit", function (event) {
     event.preventDefault();
-    //console.log( $( this ).serialize() );
-    //console.log($("#tweet-text").val());
     let tweetText = $("#tweet-text").val();
-    if (tweetText === "" || tweetText === undefined || tweetText === null) {
-      //alert("Your input is empty!");
+    if (!tweetText) {
       showErrorMessage("Your input is empty!");
     } else if (tweetText.length > 140) {
       showErrorMessage("Your input is more than 140 charactor limit!");
-      //alert("Your input is more than 140 charactor limit!");
     } else {
-      $.post(url, $(this).serialize());
-      if (!$(".error-message").is(":hidden")) {
-        $(".error-message").slideUp("slow", function () {
-          location.reload();
-        });
-      } else {
-        location.reload();
-      }
+      $.post(url, $(this).serialize(),function(){
+        if (!$(".error-message").is(":hidden")) {
+          $(".error-message").slideUp("slow");
+        }
+        loadNewTweet(url);
+      });
+      $(this).trigger("reset");
     }
   });
 });
